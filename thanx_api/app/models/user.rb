9ecee76fc5a4bf5a4cb00354bd64966f38f2ed1b
wujibear <hello@wujibear.studio>
2025-07-11 16:08:29 -0700

    run authentication migrations

--- thanx_api/app/models/user.rb
+++ thanx_api/app/models/user.rb
@@ -4,3 +4,18 @@ class User < ApplicationRecord
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email_address   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email_address  (email_address) UNIQUE
+#
